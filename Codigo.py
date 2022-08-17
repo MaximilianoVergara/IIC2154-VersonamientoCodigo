6edb48116252dea8ce8c31201f5871da7d441238
MaximilianoVergara <mvvergara1@uc.cl>
2022-08-17 04:47:25 -0400

    v1

---
+++ Codigo.py
@@ -0,0 +1,26 @@
+def main():
+    top_usuarios = function2
+    top_tweets = function1
+    top_dias = function3
+    top_hastags = function4
+
+    return top_usuarios
+
+def function1(diccionario):
+    lista = []
+
+    return lista
+
+def function2(diccionario):
+    lista = []
+    return lista
+
+def function3(diccionario):
+    lista = []
+    return lista
+
+def function4(diccionario):
+    lista = []
+    return lista
+
+
